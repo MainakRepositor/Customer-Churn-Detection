83c8c3d7182e91b2d07138de1c8f0726d03bc2d8
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2023-12-03 11:59:35 +0530

    Update predict.py

--- Tabs/predict.py
+++ Tabs/predict.py
@@ -26,26 +26,30 @@ def app(df, X, y):
     st.subheader("Select Values:")
 
     # Take input of features from the user.
-    gen = st.slider("Gender", int(df["gender"].min()), int(df["gender"].max()))
-    sec = st.slider("Senior Citizen", int(df["SeniorCitizen"].min()), int(df["SeniorCitizen"].max()))
-    par = st.slider("Partner", int(df["Partner"].min()), int(df["Partner"].max()))
-    dep = st.slider("Dependents", int(df["Dependents"].min()), int(df["Dependents"].max()))
-    ten = st.slider("Tenure", int(df["tenure"].min()), int(df["tenure"].max()))
-    phn = st.slider("Phone Service", int(df["PhoneService"].min()), int(df["PhoneService"].max()))
-    mul = st.slider("Multiple Lines", int(df["MultipleLines"].min()), int(df["MultipleLines"].max()))
-    its = st.slider("Internet Service", int(df["InternetService"].min()), int(df["InternetService"].max()))
-    osr = st.slider("Online Security", int(df["OnlineSecurity"].min()), int(df["OnlineSecurity"].max()))
-    obp = st.slider("Online Backup", int(df["OnlineBackup"].min()), int(df["OnlineBackup"].max()))
-    dvp = st.slider("Device Protection", int(df["DeviceProtection"].min()), int(df["DeviceProtection"].max()))
-    tsp = st.slider("Tech Support", int(df["TechSupport"].min()), int(df["TechSupport"].max()))
-    stv = st.slider("Streaming TV", int(df["StreamingTV"].min()), int(df["StreamingTV"].max()))
-    smv = st.slider("Streaming Movies", int(df["StreamingMovies"].min()), int(df["StreamingMovies"].max()))
-    cot = st.slider("Contract", int(df["Contract"].min()), int(df["Contract"].max()))
-    pbl = st.slider("Paperless Billing", int(df["PaperlessBilling"].min()), int(df["PaperlessBilling"].max()))
-    pym = st.slider("Payment Method", int(df["PaymentMethod"].min()), int(df["PaymentMethod"].max()))
-    mnc = st.slider("Monthly Charges", float(df["MonthlyCharges"].min()), float(df["MonthlyCharges"].max()))
-    toc = st.slider("Total Charges", float(df["TotalCharges"].min()), float(df["TotalCharges"].max()))
-       
+    col1,col2 = st.columns(2)
+    with col1:
+        gen = st.slider("Gender", int(df["gender"].min()), int(df["gender"].max()))
+        sec = st.slider("Senior Citizen", int(df["SeniorCitizen"].min()), int(df["SeniorCitizen"].max()))
+        par = st.slider("Partner", int(df["Partner"].min()), int(df["Partner"].max()))
+        dep = st.slider("Dependents", int(df["Dependents"].min()), int(df["Dependents"].max()))
+        ten = st.slider("Tenure", int(df["tenure"].min()), int(df["tenure"].max()))
+        phn = st.slider("Phone Service", int(df["PhoneService"].min()), int(df["PhoneService"].max()))
+        mul = st.slider("Multiple Lines", int(df["MultipleLines"].min()), int(df["MultipleLines"].max()))
+        its = st.slider("Internet Service", int(df["InternetService"].min()), int(df["InternetService"].max()))
+        osr = st.slider("Online Security", int(df["OnlineSecurity"].min()), int(df["OnlineSecurity"].max()))
+        obp = st.slider("Online Backup", int(df["OnlineBackup"].min()), int(df["OnlineBackup"].max()))
+
+    with col2:
+        dvp = st.slider("Device Protection", int(df["DeviceProtection"].min()), int(df["DeviceProtection"].max()))
+        tsp = st.slider("Tech Support", int(df["TechSupport"].min()), int(df["TechSupport"].max()))
+        stv = st.slider("Streaming TV", int(df["StreamingTV"].min()), int(df["StreamingTV"].max()))
+        smv = st.slider("Streaming Movies", int(df["StreamingMovies"].min()), int(df["StreamingMovies"].max()))
+        cot = st.slider("Contract", int(df["Contract"].min()), int(df["Contract"].max()))
+        pbl = st.slider("Paperless Billing", int(df["PaperlessBilling"].min()), int(df["PaperlessBilling"].max()))
+        pym = st.slider("Payment Method", int(df["PaymentMethod"].min()), int(df["PaymentMethod"].max()))
+        mnc = st.slider("Monthly Charges", float(df["MonthlyCharges"].min()), float(df["MonthlyCharges"].max()))
+        toc = st.slider("Total Charges", float(df["TotalCharges"].min()), float(df["TotalCharges"].max()))
+           
 
 
     # Create a list to store all the features
